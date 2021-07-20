03e414258020af733b724830dd05e9796f484aee
saur1n <dr.saurin.parikh@gmail.com>
2021-07-20 18:18:26 -0400

    minor updates

--- img2cs.m
+++ img2cs.m
@@ -7,40 +7,11 @@
 %   image analysis heart
 %   dr.saurin.parikh@gmail.com
 
-function img2cs(files, dimensions, params)
+function img2cs(files, dimensions, params, justanalyze)
     
-    all = zeros(1, size(files, 1));
-    for ii = 1 : size(all, 2)
-        all(ii) = exist(strcat(files{ii}, '.binary'));
-    end
-    pos = find(all==0);
-    
-    if isempty(pos)
-        disp('All files are already analyzed.')
-        if input('Do you want to re-analyze them? [Y/N] ', 's') == 'Y'
-            analyze_directory_of_images(files, params{:} );
-            direct_upload = 'N';
-        else
-            direct_upload = 'Y';
-        end
+    if justanalyze == 'Y'
+        analyze_directory_of_images(files, params{:} );
     else
-        fprintf('%d out of %d images remain to be analyzed.\n',...
-            length(pos),...
-            length(files))
-        if input('Do you want to re-analyze all? [Y/N] ', 's') == 'Y'
-            analyze_directory_of_images(files, params{:} );
-            direct_upload = 'N';
-        else
-%             files2 = files(pos);
-%             analyze_directory_of_images(files2, params{:} );
-            direct_upload = 'N';
-        end
-    end
-
-    if direct_upload == 'N'
-%%  COLLECT IMAGES WITH NO GRID
-%   Those images that weren't analyzed correctly
-
         all = zeros(1, size(files, 1));
         for ii = 1 : size(all, 2)
             all(ii) = exist(strcat(files{ii}, '.binary'));
@@ -48,34 +19,88 @@ function img2cs(files, dimensions, params)
         pos = find(all==0);
 
         if isempty(pos)
-            disp('All images were successfully analyzed.')
-        else
-            fprintf('%d image/s were not analyzed.\n',length(pos))
-            alt_thresh = input('Would you like to re-analyze all images using a different background threshold? [Y/N] ', 's');
-            if alt_thresh == 'Y'
-                thresh = input('New threshold (default = 1.25): ');
-                params = { ...
-                    'parallel', true, ...
-                    'verbose', true, ...
-                    'grid', OffsetAutoGrid('dimensions', dimensions), ... default
-                    'threshold', BackgroundOffset('offset', thresh) };
+            disp('All files are already analyzed.')
+            if input('Do you want to re/analyze them? [Y/N] ', 's') == 'Y'
                 analyze_directory_of_images(files, params{:} );
+                direct_upload = 'N';
+            else
+                direct_upload = 'Y';
+            end
+        else
+            fprintf('%d out of %d images remain to be analyzed.\n',...
+                length(pos),...
+                length(files))
+            if input('Do you want to re-analyze all? [Y/N] ', 's') == 'Y'
+                analyze_directory_of_images(files, params{:} );
+                direct_upload = 'N';
+            else
+    %             files2 = files(pos);
+    %             analyze_directory_of_images(files2, params{:} );
+                direct_upload = 'N';
+            end
+        end
 
-                all = zeros(1, size(files, 1));
-                for ii = 1 : size(all, 2)
-                    all(ii) = exist(strcat(files{ii}, '.binary'));
-                end
-                pos2 = find(all==0);
-                if isempty(pos2)
-                    disp('All images were successfully analyzed with the new threshold.')
+        if direct_upload == 'N'
+    %%  COLLECT IMAGES WITH NO GRID
+    %   Those images that weren't analyzed correctly
+    
+            all = zeros(1, size(files, 1));
+            for ii = 1 : size(all, 2)
+                all(ii) = exist(strcat(files{ii}, '.binary'));
+            end
+            pos = find(all==0);
+    
+            if isempty(pos)
+                disp('All images were successfully analyzed.')
+            else
+                fprintf('%d image/s were not analyzed.\n',length(pos))
+                alt_thresh = input('Would you like to re-analyze all images using a different background threshold? [Y/N] ', 's');
+                if alt_thresh == 'Y'
+                    thresh = input('New threshold (default = 1.25): ');
+                    params = { ...
+                        'parallel', true, ...
+                        'verbose', true, ...
+                        'grid', OffsetAutoGrid('dimensions', dimensions), ... default
+                        'threshold', BackgroundOffset('offset', thresh) };
+                    analyze_directory_of_images(files, params{:} );
+    
+                    all = zeros(1, size(files, 1));
+                    for ii = 1 : size(all, 2)
+                        all(ii) = exist(strcat(files{ii}, '.binary'));
+                    end
+                    pos2 = find(all==0);
+                    if isempty(pos2)
+                        disp('All images were successfully analyzed with the new threshold.')
+                    else
+                        fprintf('%d image/s were not analyzed.\n Manually place grid using previous threshold\n',length(pos))
+                        for ii = 1 : length(pos)
+                            analyze_image( files{pos(ii)}, params{:}, ...
+                                'grid', ManualGrid('dimensions', dimensions), 'threshold', BackgroundOffset('offset', 1.25));
+                        end
+                    end
                 else
-                    fprintf('%d image/s were not analyzed.\n Manually place grid using previous threshold\n',length(pos))
+                    disp('Manually place grid on images')
                     for ii = 1 : length(pos)
                         analyze_image( files{pos(ii)}, params{:}, ...
                             'grid', ManualGrid('dimensions', dimensions), 'threshold', BackgroundOffset('offset', 1.25));
                     end
                 end
+            end
+        end
+        
+        fprintf('Examine binary images to verify proper colony detection before going forward.\nPress enter to proceed.\n')
+        pause
+        
+        if input('Are the images properly analyzed? [Y/N]: ', 's') == 'N'
+            if input('Is there a problem with all of them? [Y/N]: ', 's') == 'Y'
+                disp('Manually place grid on all images')
+                for ii = 1 : length(files)
+                    analyze_image( files{ii}, params{:}, ...
+                        'grid', ManualGrid('dimensions', dimensions), 'threshold', BackgroundOffset('offset', 1.25));
+                end
             else
+                pos = input('Problematic images: ');
+                
                 disp('Manually place grid on images')
                 for ii = 1 : length(pos)
                     analyze_image( files{pos(ii)}, params{:}, ...
@@ -84,26 +109,4 @@ function img2cs(files, dimensions, params)
             end
         end
     end
-    
-    fprintf('Examine binary images to verify proper colony detection before going forward.\nPress enter to proceed.\n')
-    pause
-    
-    if input('Are the images properly analyzed? [Y/N]: ', 's') == 'N'
-        if input('Is there a problem with all of them? [Y/N]: ', 's') == 'Y'
-            disp('Manually place grid on all images')
-            for ii = 1 : length(files)
-                analyze_image( files{ii}, params{:}, ...
-                    'grid', ManualGrid('dimensions', dimensions), 'threshold', BackgroundOffset('offset', 1.25));
-            end
-        else
-            pos = input('Problematic images: ');
-            
-            disp('Manually place grid on images')
-            for ii = 1 : length(pos)
-                analyze_image( files{pos(ii)}, params{:}, ...
-                    'grid', ManualGrid('dimensions', dimensions), 'threshold', BackgroundOffset('offset', 1.25));
-            end
-        end
-    end
-    
 end
--- initialize_collection.m
+++ initialize_collection.m
@@ -1,15 +1,37 @@
 
 
-    tablename_collection = 'DELETION_BY4741_SPACE_AGAR';
-    collection = fetch(conn, sprintf(['select * ',...
-        'from %s order by 384plate asc, 384col asc, 384row asc'], tablename_collection));
+%     tablename_collection = 'BARFLEX_SPACE_AGAR';
+%     collection = fetch(conn, sprintf(['select * ',...
+%         'from %s order by 384plate asc, 384col asc, 384row asc'], tablename_collection));
+%     n_ref = 5;
+    
+    collection = fetch(conn, ['select * from BARFLEX_SPACE_AGAR_180313 ',...
+        'union ',...
+        'select * from PROTOGENE_COLLECTION']);
+    plate384 = fetch(conn, 'select * from PLATE384');
     n_ref = 5;
     
+    n_col_plates = length(unique(collection.x384plate_1(~isnan(collection.x384plate_1))));
     
-    for k = 1:length(unique(collection.x384plate_1))
-      data{k} = col2grid(collection.strain_id(collection.x384plate_1 == k));
+    for k = 1:n_col_plates
+%       data{k} = col2grid(collection.strain_id(collection.x384plate_1 == k));
+      temp = collection(collection.x384plate_1 == k,:);
+      temp = outerjoin(temp, plate384, 'Keys', {'x384col_1', 'x384row_1'},...
+        'Type', 'right','MergeKeys',true);
+      temp = sortrows(temp,'pos','ascend');
+      data{k} = col2grid(temp.strain_id);
     end
     
-    for kk = (length(unique(collection.x384plate_1)) + 1):(length(unique(collection.x384plate_1)) + n_ref)
+    for kk = (n_col_plates + 1):(n_col_plates + n_ref)
         data{kk} = ones(16,24)*-1;
     end
+    
+    
+
+    
+    
+    
+
+
+   
+    
